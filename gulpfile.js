661317ad790e5f53e98f08e6d573622543e42c77
the_nepodarok <kittyrat@yandex.ru>
2022-05-17 23:34:45 +0300

    project finalized

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
 const terser = require("gulp-terser");
 const imagemin = require("gulp-imagemin");
-const svgstore = require("gulp-svgstore");
 const del = require("del");
 const sync = require("browser-sync").create();
 const webp = require("gulp-webp");
@@ -22,14 +21,57 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// HTML Min
+
+const htmlMin = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: false }))
+    .pipe(gulp.dest("build"));
+}
+
+// Scripts
+
+const scriptsMin = () => {
+  return gulp.src("source/js/**/*.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scriptsMin = scriptsMin;
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = optimizeImages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = copyImages;
 
 // WebP
 
@@ -41,12 +83,34 @@ const createWebp = () => {
 
 exports.createWebp = createWebp;
 
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+}
+
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -57,13 +121,51 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/js/*.js", gulp.series(scriptsMin))
+  gulp.watch("source/*.html", gulp.series(htmlMin, reload));
 }
 
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    htmlMin,
+    scriptsMin,
+    createWebp
+  ),
+);
+
+exports.build = build;
+
+// Default
+
 exports.default = gulp.series(
-  styles, server, watcher
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    htmlMin,
+    scriptsMin,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
 );
--- source/javascript/example.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.example-change__toggle');
-const catFat = document.querySelector('.example-change__area--fat');
-const catSkinny = document.querySelector('.example-change__area--skinny');
-
-toggle.addEventListener('click');
-
-
-
